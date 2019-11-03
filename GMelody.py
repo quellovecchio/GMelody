4378af29bedfef5628b343f77587032ece1d5d05
quellovecchio <l.delvecchio@hotmail.com>
2019-11-03 17:49:35 +0100

    Started working on the music generation

--- GMelody.py
+++ GMelody.py
@@ -15,7 +15,129 @@
 
 from __future__ import print_function
 from MidiCoordinator import MidiCoordinator
+import tensorflow as tf
 
-print("a")
-mc = MidiCoordinator(0,120)
-print(mc.midiToMatrix("abba.mid"))
+class GMelody():
+
+    def __init__(self):
+
+        self.midi_notes = 88
+        self.midi_ticks = 17
+        self.midi_shape = (self.midi_notes, self.midi_ticks)
+        self.latent_dim = 100
+
+        optimizer = Adam(0.0002, 0.5)
+
+         # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # The generator takes noise as input and generates imgs
+        z = Input(shape=(self.latent_dim,))
+        result = self.generator(z)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated images as input and determines validity
+        validity = self.discriminator(result)
+
+        # The combined model  (stacked generator and discriminator)
+        # Trains the generator to fool the discriminator
+        self.combined = Model(z, validity)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+
+    def build_generator(self):
+
+        model = Sequential()
+
+        model.add(Dense(256, input_dim=self.latent_dim))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.midi_shape), activation='tanh'))
+        model.add(Reshape(self.midi_shape))
+        model.summary()
+
+        noise = Input(shape=(self.latent_dim,))
+        result = model(noise)
+
+        return Model(noise, result)
+
+
+    def build_discriminator(self):
+
+        model = Sequential()
+
+        model.add(Flatten(input_shape=self.midi_shape))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(256))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+
+        result = Input(shape=self.midi_shape)
+        validity = model(result)
+
+        return Model(result, validity)
+
+
+    def train(self, epochs, batch_size, sample_interval=50):
+
+        # Load the dataset
+        # This is code for testing actually
+        mc = MidiCoordinator(24,102)
+        matrix = mc.midiToMatrix("2.mid")
+        data = tf.data.Dataset.from_tensor_slices(matrix)
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random batch of midis
+            idx = np.random.randint(0, data.shape[0], batch_size)
+            phrases = data[idx]
+
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+
+            # Generate a batch of new images
+            gen_phrases = self.generator.predict(noise)
+
+            # Train the discriminator
+            d_loss_real = self.discriminator.train_on_batch(phrases, valid)
+            d_loss_fake = self.discriminator.train_on_batch(gen_phrases, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+
+            # Train the generator (to have the discriminator label samples as valid)
+            g_loss = self.combined.train_on_batch(noise, valid)
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_phrases(epoch)
---
+++ MusicGenerator.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from tensorflow.python.ops import control_flow_ops
+from tqdm import tqdm
+import numpy as np
+
+class MusicGenerator(object):
+
+    def __init__(self):
+        #self.img_rows = 28
+        #self.img_cols = 28
+        #self.channels = 1
+        #self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        #self.latent_dim = 100
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # The generator takes noise as input and generates imgs
+        z = Input(shape=(self.latent_dim,))
+        img = self.generator(z)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated images as input and determines validity
+        validity = self.discriminator(img)
+
+        # The combined model  (stacked generator and discriminator)
+        # Trains the generator to fool the discriminator
+        self.combined = Model(z, validity)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+    def build_generator(self):
+
+        model = Sequential()
+
+        model.add(Dense(256, input_dim=self.latent_dim))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(Reshape(self.img_shape))
+
+        model.summary()
+
+        noise = Input(shape=(self.latent_dim,))
+        img = model(noise)
+
+        return Model(noise, img)
