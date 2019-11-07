6137c29346b79c8f18d7c2e5343879df9a958366
quellovecchio <l.delvecchio@hotmail.com>
2019-11-07 17:27:44 +0100

    Code update

--- GMelody.py
+++ GMelody.py
@@ -13,9 +13,24 @@
 #
 #   -----------------------------------------------------------------
 
-from __future__ import print_function
-from MidiCoordinator import MidiCoordinator
+from __future__ import print_function, division
+
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+
 import tensorflow as tf
+
+from MidiCoordinator import MidiCoordinator
+
+import sys
+
+import numpy as np
 
 class GMelody():
 
@@ -113,8 +128,9 @@ class GMelody():
             # ---------------------
 
             # Select a random batch of midis
-            idx = np.random.randint(0, data.shape[0], batch_size)
-            phrases = data[idx]
+            #idx = np.random.randint(0, data.shape[0], batch_size)
+            # 0 in the next line has to be substituted with idx, this is for testing purposes only
+            phrases = np.array([data])
 
             noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
 
@@ -122,7 +138,7 @@ class GMelody():
             gen_phrases = self.generator.predict(noise)
 
             # Train the discriminator
-            d_loss_real = self.discriminator.train_on_batch(phrases, valid)
+            d_loss_real = self.discriminator.train_on_batch(phrases, valid, sample_weight=None, class_weight=None, reset_metrics=True)
             d_loss_fake = self.discriminator.train_on_batch(gen_phrases, fake)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
@@ -140,6 +156,29 @@ class GMelody():
 
             # If at save interval => save generated image samples
             if epoch % sample_interval == 0:
-                self.sample_phrases(epoch)
+                self.sample_midi(epoch)
 
             # need to start working on the music generation
+    
+    def sample_midi(self, epoch):
+
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
+        gen_midi = self.generator.predict(noise)
+
+        # Rescale images 0 - 1
+        gen_midi = 0.5 * gen_midi + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_midi[cnt, :,:,0], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("generated/%d.mid" % epoch)
+        plt.close()
+
+if __name__ == '__main__':
+    g = GMelody()
+    g.train(epochs=30000, batch_size=1, sample_interval=200)
