42bff0595131607573515bcceb6dfe886b4326b0
quellovecchio <l.delvecchio@hotmail.com>
2020-04-17 09:44:31 +0000

    Reverted last change

--- GMelody.py
+++ GMelody.py
@@ -60,7 +60,7 @@ class GMelody():
         result = self.generator(z)
 
         # For the combined model we will only train the generator
-        self.discriminator.trainable = True
+        self.discriminator.trainable = False
 
         # The discriminator takes generated images as input and determines validity
         validity = self.discriminator(result)
